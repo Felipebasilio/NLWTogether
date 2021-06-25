ef651939b0592cdd83386e0b5df85dff378e68bc
Felipebasilio <felipe.basilio@avenuecode.com>
2021-06-25 13:03:50 -0300

    feat: Finished class 2 - useContext, useEffect useState and useAuth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { Button } from "./components"
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext"
 
 function App() {
+
   return (
-    <Button text="Botão 1" />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,12 @@
-type ButtonProps = {
-    text: string;
-}
+import { ButtonHTMLAttributes } from "react";
+
+import "../styles/button.scss";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button = (props: ButtonProps) => {
     return(
-        <button>{props.text || "Default"}</button>
+        <button className="button" {...props} />
     )
 }
 
